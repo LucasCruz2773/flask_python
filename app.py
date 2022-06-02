1aaa715325e1fe0242ae826ecb9e463ef81ee955
LucasCruz2773 <lucascruz2773@gmail.com>
2022-06-01 21:38:05 -0300

    Feat #4: Visualização de tarefas

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ migrate = Migrate(app, db)
 
 # application models.
 
-
 class Task(db.Model):
     """model to store a task data"""
 
@@ -42,57 +41,133 @@ class Task(db.Model):
 
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     """:type : datetime"""
+
+    user_id = db.Column(db.Integer, nullable=False)
+    """:type : str"""
  
     def __repr__(self):
         """override __repr__ method"""
-        return f"Task: #{self.id}, content: {self.description, self.situation, self.spend_hours, self.category}"
+        return f"Task: #{self.id}, content: {self.description, self.situation, self.spend_hours, self.category, self.user_id}"
+
+class User(db.Model):
+    """model to store a user data"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    """:type : int"""
+
+    login = db.Column(db.String(200), nullable=False)
+    """:type : str"""
+
+    password = db.Column(db.String(200), nullable=False)
+    """:type : str"""
+
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    """:type : datetime"""
+ 
+    def __repr__(self):
+        """override __repr__ method"""
+        return f"Task: #{self.id}, content: {self.login, self.password}"
 
 # routes and handlers.
 
+
+# user1 = User(login="Lucas", password="123123")
+# user2 = User(login="Victor", password="coxinha123")
+# try:
+#     db.session.add(user1)
+#     db.session.add(user2)
+#     db.session.commit()
+    
+# except:
+#     print("Erro ao cadastrar usuários")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """root route"""
     if request.method == 'POST':
-        task = Task(description=request.form['description'], situation=request.form['situation'], spend_hours=request.form['spend_hours'], category=request.form['category'])
+        taskCreate = Task(description=request.form['description'], situation=request.form['situation'], spend_hours=request.form['spend_hours'], category=request.form['category'], user_id=int(request.form['user_id']))
         try:
-            db.session.add(task)
+            db.session.add(taskCreate)
             db.session.commit()
-            return redirect('/')
+            tasks = Task.query.order_by(Task.date_created).all()
+            categories = []
+            for task in tasks:
+                if task.user_id == int(request.form['user_id']):
+                    addCategory = True
+                    for category in categories:
+                        if(category['name'] == task.category):
+                            category['hours'] += task.spend_hours
+                            addCategory = False
+                    if(addCategory):
+                        categories.append({
+                            'name': task.category,
+                            'hours': task.spend_hours
+                        })
+            return render_template('index.html', tasks=tasks, categories=categories, login=int(request.form['user_id']))
         except:
             return "Houve um erro, ao inserir a tarefa"
     else:
-        tasks = Task.query.order_by(Task.date_created).all()
-        categories = []
-        for task in tasks:
-            addCategory = True
-            for category in categories:
-                print(category)
-                if(category['name'] == task.category):
-                    category['hours'] += task.spend_hours
-                    addCategory = False
-            if(addCategory):
-                categories.append({
-                    'name': task.category,
-                    'hours': task.spend_hours
-                })
-        return render_template('index.html', tasks=tasks, categories=categories)
+        return render_template('index.html', tasks=[], categories=[], login=0)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """root route"""
+    if request.method == 'POST':
+        users = User.query.order_by(User.date_created).all()
+        findUserId = 0
+        for user in users:
+            if(user.login == request.form['login'] and user.password == request.form['password']):
+                findUserId = user.id
+            
+        if findUserId != 0:
+            tasks = Task.query.order_by(Task.date_created).all()
+            categories = []
+            for task in tasks:
+                if task.user_id == findUserId:
+                    addCategory = True
+                    for category in categories:
+                        if(category['name'] == task.category):
+                            category['hours'] += task.spend_hours
+                            addCategory = False
+                    if(addCategory):
+                        categories.append({
+                            'name': task.category,
+                            'hours': task.spend_hours
+                        })
+            return render_template('index.html', tasks=tasks, categories=categories, login=findUserId)
+        else:
+            return render_template('login.html', error="login_error")
+    else:
+        return render_template('login.html', error="no_error")
 
-@app.route('/delete/<int:id>')
-def delete(id):
+@app.route('/delete/<int:id>/<int:user_id>')
+def delete(id, user_id):
     """delete a task"""
     task = Task.query.get_or_404(id)
     try:
         db.session.delete(task)
         db.session.commit()
-        return redirect('/')
+        tasks = Task.query.order_by(Task.date_created).all()
+        categories = []
+        for task in tasks:
+            if task.user_id == int(user_id):
+                addCategory = True
+                for category in categories:
+                    if(category['name'] == task.category):
+                        category['hours'] += task.spend_hours
+                        addCategory = False
+                if(addCategory):
+                    categories.append({
+                        'name': task.category,
+                        'hours': task.spend_hours
+                    })
+        return render_template('index.html', tasks=tasks, categories=categories, login=int(user_id))
     except:
         return "Houve um erro, ao inserir a tarefa"
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
+@app.route('/update/<int:id>/<int:user_id>', methods=['GET', 'POST'])
+def update(id, user_id):
     """update route"""
     task = Task.query.get_or_404(id)
     if request.method == 'POST':
@@ -102,8 +177,22 @@ def update(id):
         task.category = request.form['category']
         try:
             db.session.commit()
-            return redirect('/')
+            tasks = Task.query.order_by(Task.date_created).all()
+            categories = []
+            for task in tasks:
+                if task.user_id == int(user_id):
+                    addCategory = True
+                    for category in categories:
+                        if(category['name'] == task.category):
+                            category['hours'] += task.spend_hours
+                            addCategory = False
+                    if(addCategory):
+                        categories.append({
+                            'name': task.category,
+                            'hours': task.spend_hours
+                        })
+            return render_template('index.html', tasks=tasks, categories=categories, login=int(user_id))
         except:
             return "Houve um erro, ao atualizar a tarefa"
     else:
-        return render_template('update.html', task=task)
+        return render_template('update.html', task=task, login=user_id)
